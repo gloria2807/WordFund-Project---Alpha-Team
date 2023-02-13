7af2395e246a2cfac9078258646b3ecf53556d4a
Gloria Enyi <nkemaconam@gmail.com>
2023-02-13 13:24:05 +0100

    JavaScript file uploaded

---
+++ home.js
@@ -0,0 +1,6 @@
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownBtn.addEventListener('click', () => {
+dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+});

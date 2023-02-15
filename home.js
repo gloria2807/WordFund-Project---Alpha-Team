bb07e80ca67b6c709d8c62973ed3056b1c65c767
Gloria Enyi <nkemaconam@gmail.com>
2023-02-15 03:03:23 -0800

    Update home.js

--- home.js
+++ home.js
@@ -4,3 +4,6 @@ const dropdownMenu = document.querySelector('.dropdown-menu');
 dropdownBtn.addEventListener('click', () => {
 dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
 });
+function myFunction() {
+    document.getElementById("mydropdown").classList.toggle("show");
+}
